cc7b1a3176d17cf38e3e13cdffdb1c5237e926fd
Helio Mendonca <hsm@fe.up.pt>
2015-04-10 19:36:39 +0100

    ledbut with pigpio

--- ledbut.py
+++ ledbut.py
@@ -1,33 +1,30 @@
-import platform
+import pigpio
 
-if platform.uname()[4] == "armv6l":
-    import RPi.GPIO as GPIO # @UnresolvedImport @UnusedImport
-else:
-    import virtualGPIO as GPIO # @Reimport
+#import platform
+#if platform.uname()[4] == "armv6l":
+#    import RPi.GPIO as GPIO # @UnresolvedImport @UnusedImport
+#else:
+#    import virtualGPIO as GPIO # @Reimport
     
 class LedBut():
     def __init__(self, server, parent=None):
         self.server = server
         
-        #to disable RuntimeWarning: This channel is already in use
-        GPIO.setwarnings(False)
-        
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setup(4, GPIO.OUT)
-        GPIO.output(4, True)
-        GPIO.setup(17, GPIO.IN)
+        self.pi = pigpio.pi()
+        self.pi.set_mode( 4, pigpio.OUTPUT) # LED
+        self.pi.set_mode(17, pigpio.INPUT)  # BUT
     
-        self.but = not GPIO.input(17)
+        self.but = not self.pi.read(17)
     
     def procBut(self):
-        b = not GPIO.input(17)
+        b = not self.pi.read(17)
         if b != self.but:
             self.but = b
             self.server.send("b1" if b else "b0")
         
     def cmdLed(self, cmd):
-        GPIO.output(4, cmd[1] == "1")
+        self.pi.write(4, cmd[1] == "1")
     
     def cmdBut(self, cmd):
-        b = not GPIO.input(17)
+        b = not self.pi.read(17)
         self.server.send("b1" if b else "b0")
